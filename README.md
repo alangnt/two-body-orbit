93a5b7b359552a6b66b77e103dd5ca185e0c4f0a
Alan Geirnaert <geirnaert.alan@icloud.com>
2026-09-22 13:54:39 +0200

    feat: locate the barycenter

---
+++ README.md
@@ -0,0 +1,3 @@
+# Two-Body Orbit
+
+$$r_n = r \left(\frac{m_2}{m_1 + m_2}\right)$$
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-# pluto charon system
+import math
 
 G = 6.6743*10e-11
-MASS_PLUTO = 1.303*10e22
-MASS_CHARON = 1.586*10e21
-MEAN_SEPARATION = 1.9596*10e7
-ORBITAL_ECCENTRICITY = 0
+MASS_PLUTO = 1.303*10e22        # (m_a)
+MASS_CHARON = 1.586*10e21       # (m_b)
+MEAN_SEPARATION = 1.9596*10e7   # (r)
+ORBITAL_ECCENTRICITY = 0        # (e)
 
-# locate the barycenter
+def calculate_distance_to_barycenter(m_a, m_b):
+    total_mass = m_a + m_b
+    return MEAN_SEPARATION * (m_b / total_mass)
+
+def main():
+    # TODO: input planets
+    
+    # locate the barycenter
+    barycenter_a = calculate_distance_to_barycenter(MASS_PLUTO, MASS_CHARON)
+    barycenter_b = calculate_distance_to_barycenter(MASS_CHARON, MASS_PLUTO)
+    
+    print(math.isclose((barycenter_a + barycenter_b), MEAN_SEPARATION))
+    
+main()
