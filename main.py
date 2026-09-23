6c6810a566c9e65757a5b0fff33ab4254934bfec
Alan Geirnaert <geirnaert.alan@icloud.com>
2026-09-23 09:46:05 +0200

    feat: calculate orbital speed for both objects

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ MASS_CHARON = 1.586*10e21       # (m_b)
 MEAN_SEPARATION = 1.9596*10e7   # (r)
 ORBITAL_ECCENTRICITY = 0        # (e)
 
+def calculate_orbital_velocity(angular_velocity, distance_to_barycenter):
+    return angular_velocity * distance_to_barycenter
+
 def calculate_angular_velocity(orbital_period):
     return (2 * math.pi) / orbital_period
 
@@ -33,8 +36,8 @@ def main():
     multiplied_mass = MASS_PLUTO * MASS_CHARON
     
     # locate the barycenter
-    barycenter_a = calculate_distance_to_barycenter(total_mass, MASS_CHARON)        # r1
-    barycenter_b = calculate_distance_to_barycenter(total_mass, MASS_PLUTO)         # r2
+    barycenter_a = calculate_distance_to_barycenter(total_mass, MASS_CHARON)        # r_a
+    barycenter_b = calculate_distance_to_barycenter(total_mass, MASS_PLUTO)         # r_b
     
     combined_grav_param = calculate_combined_gravitational_parameter(total_mass)    # µ(grav)
     reduced_mass = calculate_reduced_mass(multiplied_mass, total_mass)              # µ
@@ -46,6 +49,10 @@ def main():
     
     # we can then calculate the angular velocity
     angular_velocity = calculate_angular_velocity(orbital_period)                   # ω
+    
+    # now we calculate both orbital velocities
+    orbital_speed_a = calculate_orbital_velocity(angular_velocity, barycenter_a)    # v_a
+    orbital_speed_b = calculate_orbital_velocity(angular_velocity, barycenter_b)    # v_b
     
     print(math.isclose((barycenter_a + barycenter_b), MEAN_SEPARATION))
     
